de2ed4e30831ba7d7d90d076f2c0189d4a7bd73f
PageEllen <ellen.page@kcl.ac.uk>
2023-10-06 08:55:26 +0100

    add model thing

--- things/models.py
+++ things/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class thing(Model):
+    name = CharField(MaxLength = 30)
+    description = CharField(MaxLength = 120)
+    quantity = IntegerField()
