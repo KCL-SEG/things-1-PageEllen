936f22a75046688c6453f540bfffafa42c256a42
PageEllen <ellen.page@kcl.ac.uk>
2023-10-06 09:05:08 +0100

    changed class name to match tests

--- things/models.py
+++ things/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class thing(models.Model):
+class Thing(models.Model):
     name = models.CharField(max_length = 30)
     description = models.CharField(max_length = 120)
     quantity = models.IntegerField()
